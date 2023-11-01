29e092bf6fa5f9bd847858583d98ace1ba85b0bb
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-11-01 05:23:02 -0300

    Custom JSON Serialization

---
+++ 08_CustomJSONSerialization/rest-com-spring-boot-e-java/src/main/java/com/joseleonardo/data/dto/v1/PessoaDTO.java
@@ -0,0 +1,89 @@
+package com.joseleonardo.data.dto.v1;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "endereco", "primeiro_nome", "ultimo_nome", "genero"})
+public class PessoaDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	
+	@JsonProperty("primeiro_nome")
+	private String primeiroNome;
+	
+	@JsonProperty("ultimo_nome")
+	private String ultimoNome;
+	private String endereco;
+	
+	@JsonIgnore
+	private String genero;
+
+	public PessoaDTO() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+
+	public String getUltimoNome() {
+		return ultimoNome;
+	}
+
+	public void setUltimoNome(String ultimoNome) {
+		this.ultimoNome = ultimoNome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(endereco, genero, id, primeiroNome, ultimoNome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PessoaDTO other = (PessoaDTO) obj;
+		return Objects.equals(endereco, other.endereco) && Objects.equals(genero, other.genero)
+				&& Objects.equals(id, other.id) && Objects.equals(primeiroNome, other.primeiroNome)
+				&& Objects.equals(ultimoNome, other.ultimoNome);
+	}
+
+}
