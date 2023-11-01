d53cddee1b3bf867b009af4b05267a8da8699631
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-11-01 09:49:44 -0300

    Serializando em YML

--- 09_ContentNegotiation/rest-com-spring-boot-e-java/src/main/java/com/joseleonardo/controllers/PessoaController.java
+++ 09_ContentNegotiation/rest-com-spring-boot-e-java/src/main/java/com/joseleonardo/controllers/PessoaController.java
@@ -3,7 +3,6 @@ package com.joseleonardo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.joseleonardo.data.dto.v1.PessoaDTO;
 import com.joseleonardo.services.PessoaService;
+import com.joseleonardo.util.MediaTypeUtil;
 
 @RestController
 @RequestMapping("/api/pessoas/v1")
@@ -24,27 +24,33 @@ public class PessoaController {
 	@Autowired
 	private PessoaService pessoaService;
 	
-	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	@GetMapping(produces = { MediaTypeUtil.APPLICATION_JSON, MediaTypeUtil.APPLICATION_XML, 
+			MediaTypeUtil.APPLICATION_YML })
 	public List<PessoaDTO> listar() {
 		return pessoaService.listarTodas();
 	}
 	
 	@GetMapping(value = "/{id}", 
-			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+			produces = { MediaTypeUtil.APPLICATION_JSON, MediaTypeUtil.APPLICATION_XML, 
+					MediaTypeUtil.APPLICATION_YML })
 	public PessoaDTO buscarPorId(@PathVariable(value = "id") Long id) {
 		return pessoaService.buscarPorId(id);
 	}
 	
 	@PostMapping(
-			consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, 
-			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+			consumes = { MediaTypeUtil.APPLICATION_JSON, MediaTypeUtil.APPLICATION_XML,
+					MediaTypeUtil.APPLICATION_YML }, 
+			produces = { MediaTypeUtil.APPLICATION_JSON, MediaTypeUtil.APPLICATION_XML,
+					MediaTypeUtil.APPLICATION_YML })
 	public PessoaDTO salvar(@RequestBody PessoaDTO pessoa) {
 		return pessoaService.salvar(pessoa);
 	}
 	
 	@PutMapping(
-			consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, 
-			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+			consumes = { MediaTypeUtil.APPLICATION_JSON, MediaTypeUtil.APPLICATION_XML, 
+					MediaTypeUtil.APPLICATION_YML }, 
+			produces = { MediaTypeUtil.APPLICATION_JSON, MediaTypeUtil.APPLICATION_XML, 
+					MediaTypeUtil.APPLICATION_YML })
 	public PessoaDTO atualizar(@RequestBody PessoaDTO pessoa) {
 		return pessoaService.atualizar(pessoa);
 	}
