855399ad45d9f7ae15fa6a0b6a0e4e475cba47de
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-11-10 16:31:49 -0300

    Habilitando CORS de Forma Global

--- 12_ConfigurandoCross-Origin/rest-com-spring-boot-e-java/src/main/java/com/joseleonardo/config/WebConfig.java
+++ 12_ConfigurandoCross-Origin/rest-com-spring-boot-e-java/src/main/java/com/joseleonardo/config/WebConfig.java
@@ -2,10 +2,12 @@ package com.joseleonardo.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.joseleonardo.serialization.convertes.YamlJackson2HttpMessageConverter;
@@ -14,13 +16,26 @@ import com.joseleonardo.serialization.convertes.YamlJackson2HttpMessageConverter
 public class WebConfig implements WebMvcConfigurer {
 	
 	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+	
+	@Value("${cors.originPatterns:default}")
+	private String corsOriginPatterns = "";
 
 	@Override
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJackson2HttpMessageConverter());
 	}
-
 	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		String[] allowedOrigins = corsOriginPatterns.split(",");
+		
+		registry.addMapping("/**")
+			  //.allowedMethods("GET", "POST", "PUT");
+				.allowedMethods("*")
+				.allowedOrigins(allowedOrigins)
+				.allowCredentials(true);
+	}
+
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		// https://www.baeldung.com/spring-mvc-content-negotiation-json-xml
