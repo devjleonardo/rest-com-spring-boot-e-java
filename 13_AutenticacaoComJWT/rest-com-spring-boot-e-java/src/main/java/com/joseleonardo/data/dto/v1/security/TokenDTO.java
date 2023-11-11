806be59676f3a59935a46d176a461752af476dd6
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-11-11 20:59:38 -0300

    Criando Exceptions Customizadas e o DTO de Credenciais

---
+++ 13_AutenticacaoComJWT/rest-com-spring-boot-e-java/src/main/java/com/joseleonardo/data/dto/v1/security/TokenDTO.java
@@ -0,0 +1,100 @@
+package com.joseleonardo.data.dto.v1.security;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class TokenDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nomeDeUsuario;
+	private Boolean autenticado;
+	private Date dataCriacao;
+	private Date expiraçao;
+	private String accessToken;
+	private String refreshToken;
+
+	public TokenDTO() {
+	}
+
+	public TokenDTO(String nomeDeUsuario, Boolean autenticado, Date dataCriacao, 
+			Date expiraçao, String accessToken, String refreshToken) {
+		super();
+		this.nomeDeUsuario = nomeDeUsuario;
+		this.autenticado = autenticado;
+		this.dataCriacao = dataCriacao;
+		this.expiraçao = expiraçao;
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+	}
+
+	public String getNomeDeUsuario() {
+		return nomeDeUsuario;
+	}
+
+	public void setNomeDeUsuario(String nomeDeUsuario) {
+		this.nomeDeUsuario = nomeDeUsuario;
+	}
+
+	public Boolean getAutenticado() {
+		return autenticado;
+	}
+
+	public void setAutenticado(Boolean autenticado) {
+		this.autenticado = autenticado;
+	}
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Date getExpiraçao() {
+		return expiraçao;
+	}
+
+	public void setExpiraçao(Date expiraçao) {
+		this.expiraçao = expiraçao;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(accessToken, autenticado, dataCriacao, expiraçao, nomeDeUsuario, refreshToken);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TokenDTO other = (TokenDTO) obj;
+		return Objects.equals(accessToken, other.accessToken) && Objects.equals(autenticado, other.autenticado)
+				&& Objects.equals(dataCriacao, other.dataCriacao) && Objects.equals(expiraçao, other.expiraçao)
+				&& Objects.equals(nomeDeUsuario, other.nomeDeUsuario)
+				&& Objects.equals(refreshToken, other.refreshToken);
+	}
+
+}
--- 13_AutenticacaoComJWT/rest-com-spring-boot-e-java/src/main/java/com/joseleonardo/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 13_AutenticacaoComJWT/rest-com-spring-boot-e-java/src/main/java/com/joseleonardo/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import com.joseleonardo.exceptions.ExceptionResponse;
+import com.joseleonardo.exceptions.InvalidJwtAuthenticationException;
 import com.joseleonardo.exceptions.RequiredObjectIsNullException;
 import com.joseleonardo.exceptions.ResourceNotFoundException;
 
@@ -49,6 +50,18 @@ public class CustomizedResponseEntityExceptionHandler {
 				request.getDescription(false));
 		
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(Exception ex, 
+			WebRequest request) {
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(), 
+				ex.getMessage(), 
+				request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 
 }
