b8f0a4630fe9f0286cf08a7a79f9adf940ea13b4
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-11-22 21:51:09 -0300

    Criando o Controller Responsável Pelo Upload e Download de Arquivos

---
+++ 17_TrabalhandoComUploadEDownloadDeArquivos/rest-com-spring-boot-e-java/src/main/java/com/joseleonardo/controllers/FileController.java
@@ -0,0 +1,43 @@
+package com.joseleonardo.controllers;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.joseleonardo.data.dto.v1.UploadFileResponseDTO;
+import com.joseleonardo.services.FileStorageService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api/files/v1")
+@Tag(name = "File Endpoint")
+public class FileController {
+
+	private Logger logger = Logger.getLogger(FileController.class.getName());
+	
+	@Autowired
+	private FileStorageService fileStorageService;
+	
+	@PostMapping("/upload-file")
+	public UploadFileResponseDTO uploadFile(@RequestParam("file") MultipartFile file) {
+		logger.info("Armazenando arquivo em disco");
+		
+		String filename = fileStorageService.storeFile(file);
+		
+		String filedDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+				.path("/api/files/v1/download-file/")
+		        .path(filename)
+				.toUriString();
+		
+		return new UploadFileResponseDTO(
+				filename, filedDownloadUri, file.getContentType(), file.getSize());
+	}
+	
+}
