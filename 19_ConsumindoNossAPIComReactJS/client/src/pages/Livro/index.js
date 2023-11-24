e9edff04a5aa8370e187220e003218f71914f17d
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-11-24 15:32:34 -0300

    Implementando o Cabeçalho da Tela de Livros

--- 19_ConsumindoNossAPIComReactJS/client/src/pages/Livro/index.js
+++ 19_ConsumindoNossAPIComReactJS/client/src/pages/Livro/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower } from 'react-icons/fi';
 
 import "./styles.css";
 
+import logoImage from '../../assets/logo.svg'
+
 export default function Livro() {
     return (
-        <h1>Livro</h1>
+        <div className="livro-container">
+            <header>
+                <img src={logoImage} alt= "Logo" />
+
+                <span>Bem-vindo, <strong>Leandro</strong>! </span>
+
+                <Link className="button" to="livros/novo">Adicionar novo livro</Link>
+            
+                <button type="button">
+                    <FiPower size={18} color="#251FC5" />
+                </button>
+            </header>
+        </div>
     );
 }
