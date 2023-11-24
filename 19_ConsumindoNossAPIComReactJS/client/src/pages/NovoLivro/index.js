cf2d349bfbaab54cf3579520502bc2fb33c97cbd
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-11-24 18:53:31 -0300

    Integrando a Tela de Cadastro de Livros à API

--- 19_ConsumindoNossAPIComReactJS/client/src/pages/NovoLivro/index.js
+++ 19_ConsumindoNossAPIComReactJS/client/src/pages/NovoLivro/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from "../../services/api";
 
 import "./styles.css";
 
 import logoImage from '../../assets/logo.svg'
 
 export default function NovoLivro() {
+    const [id, setId] = useState(null);
+    const [titulo, setTitulo] = useState('');
+    const [autor, setAutor] = useState('');
+    const [dataLancamento, setDataLancamento] = useState('');
+    const [preco, setPreco] = useState('');
+    
+    const accessToken = localStorage.getItem("accessToken");
+
+    const navigate = useNavigate();
+
+    async function criarNovoLivro(e) {
+        e.preventDefault();
+
+        const data = {
+            titulo,
+            autor,
+            dataLancamento,
+            preco
+        };
+
+        try {
+            await api.post("api/livros/v1", data, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            navigate("/livros");
+        } catch (error) {
+            alert("Erro ao recuperar adicionar o livro ! Tente novamente!");
+        }
+    }
+
     return (
         <div className="novo-livro-container">
             <div className="content">
@@ -23,14 +58,30 @@ export default function NovoLivro() {
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Título" />
+                <form onSubmit={criarNovoLivro}>
+                    <input
+                        placeholder="Título" 
+                        value={titulo}
+                        onChange={e => setTitulo(e.target.value)}
+                    />
 
-                    <input placeholder="Autor" />
+                    <input
+                        placeholder="Autor" 
+                        value={autor}
+                        onChange={e => setAutor(e.target.value)}
+                    />
 
-                    <input type="date" />
-                    
-                    <input placeholder="Preço" />
+                    <input
+                        type="date" 
+                        value={dataLancamento}
+                        onChange={e => setDataLancamento(e.target.value)}
+                    />
+
+                    <input
+                        placeholder="Preço" 
+                        value={preco}
+                        onChange={e => setPreco(e.target.value)}
+                    />
 
                     <button className="button" type="submit">Adicionar</button>
                 </form>
--- 19_ConsumindoNossAPIComReactJS/server/src/main/java/com/joseleonardo/config/SecurityConfig.java
+++ 19_ConsumindoNossAPIComReactJS/server/src/main/java/com/joseleonardo/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,6 +36,7 @@ public class SecurityConfig {
                     .requestMatchers("/api/**").authenticated()
                     .requestMatchers("/users").denyAll()
         	)
+        	.cors(Customizer.withDefaults())
         	.apply(new JwtConfigurer(jwtTokenProvider));
     	
     	return httpSecurity.build();
