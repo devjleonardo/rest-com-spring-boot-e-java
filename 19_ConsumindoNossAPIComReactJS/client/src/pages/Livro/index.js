0beda819674473459a6865f72438af83e208e157
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-11-24 15:45:46 -0300

    Implementando a Listagem da Tela de Livros

--- 19_ConsumindoNossAPIComReactJS/client/src/pages/Livro/index.js
+++ 19_ConsumindoNossAPIComReactJS/client/src/pages/Livro/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
 
 import "./styles.css";
 
@@ -20,6 +20,54 @@ export default function Livro() {
                     <FiPower size={18} color="#251FC5" />
                 </button>
             </header>
+
+            <h1>Livros Registrados</h1>
+
+            <ul>
+                <li>
+                    <strong>Título</strong>
+                    <p>Docker Deep Dive</p>
+
+                    <strong>Autor:</strong>
+                    <p>Nigel Poulton</p>
+
+                    <strong>Preço:</strong>
+                    <p>R$ 47,90</p>
+
+                    <strong>Data de lançamento:</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5" />
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5" />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Título</strong>
+                    <p>Docker Deep Dive</p>
+
+                    <strong>Autor:</strong>
+                    <p>Nigel Poulton</p>
+
+                    <strong>Preço:</strong>
+                    <p>R$ 47,90</p>
+
+                    <strong>Data de lançamento:</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5" />
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5" />
+                    </button>
+                </li>
+            </ul>
         </div>
     );
 }
